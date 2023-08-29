5267386c80bf796428427015401123556a90c9c8
Chase LaBarre <86638507+ObfuscatedFuture@users.noreply.github.com>
2023-08-29 14:08:29 -0400

    Added Maven

---
+++ src/main/java/main.java
@@ -0,0 +1,18 @@
+package src.main.java;
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+public class main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}

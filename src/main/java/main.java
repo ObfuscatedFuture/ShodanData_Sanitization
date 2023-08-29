d3e4ee55c67c1b4b7fd2a2c03afd08a3ed383193
Chase LaBarre <86638507+ObfuscatedFuture@users.noreply.github.com>
2023-08-29 16:54:42 -0400

    WIP

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -9,6 +9,28 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
+import com.maxmind.geoip2.WebServiceClient;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import com.maxmind.geoip2.model.CityResponse;
+import com.maxmind.geoip2.model.CountryResponse;
+import com.maxmind.geoip2.record.City;
+import com.maxmind.geoip2.record.Country;
+import com.maxmind.geoip2.record.Location;
+import com.maxmind.geoip2.record.Subdivision;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+import java.io.*;
+import java.net.*;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class main
 {
@@ -17,7 +39,7 @@ public class main
         ObjectMapper objectMapper = new ObjectMapper();
         System.out.println("ShodanData_Sanitization by: Obfuscated Future");
 
-
+        WebServiceClient client = new WebServiceClient.Builder(690395, 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.host("geolite.info").build();
         FileReader reader = null;
         try {
             reader = new FileReader("/Users/chase/IdeaProjects/ShodanData_Sanitization/Traffic_cams.json");
